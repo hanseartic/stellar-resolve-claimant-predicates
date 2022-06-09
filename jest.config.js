93e3ee62f14d84b56b1c81b5bc19fb48f25fd610
Paul Schütte <paul@hanseartic.de>
2022-06-09 13:58:12 +0200

    Use babel to transform typescript

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.ts?$': 'babel-jest',
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
